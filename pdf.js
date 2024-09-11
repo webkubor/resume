9580132e4c71b956efdac6eb2f9625d83b6854cd
webkubor <webkubor@163.com>
2024-09-11 18:09:35 +0800

    feat   提交配置

--- pdf.js
+++ pdf.js
@@ -1,12 +1,23 @@
-const puppeteer = require('puppeteer');
+import puppeteer from 'puppeteer';
 
 (async () => {
-  const browser = await  puppeteer.launch() 
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  console.log('开始前往网站....')
-  await page.goto('https://web-kubor.gitee.io/resume/', { waitUntil: 'domcontentloaded'});
-  await page.emulateMedia('screen');
-  console.log('获取成功,正在导出PDF....')
-  await page.pdf({path: '.public/resume.pdf',printBackground:true, format: 'A4',scale: 0.7,margin:{top: '20px',left: '20px',bottom: '20px',right: '20px'}}); // create a PDF
+  console.log('开始前往网站....');
+  await page.goto('http://localhost:5173/resume/', { waitUntil: 'domcontentloaded' });
+  await page.emulateMediaType('screen');  // 更新为 `emulateMediaType`
+  console.log('获取成功,正在导出PDF....');
+  await page.pdf({
+    path: 'public/resume.pdf',
+    printBackground: true,
+    format: 'A4',
+    scale: 0.7,
+    margin: {
+      top: '20px',
+      left: '20px',
+      bottom: '20px',
+      right: '20px'
+    }
+  }); // create a PDF
   await browser.close();
 })();
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/resume/",
-  publicDir: "/resume/", 
+  // base: "/resume/",
+  // publicDir: "/resume/", 
+  base: process.env.NODE_ENV === "development" ? "/" : "./",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
