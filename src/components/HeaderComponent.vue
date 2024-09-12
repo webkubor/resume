6f3a6665af668adccef359866799e8773af855f4
webkubor <webkubor@163.com>
2024-09-12 11:12:49 +0800

    Feat 代码更新优化

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,24 +3,24 @@
     <header class="content-hd">
       <section class="title">
         <div class="name">
-          <h1>{{ name }}</h1>
+          <h1>{{ User.name }}</h1>
           <span class="last-modified">最后更新于{{ lastUpdated }}</span>
         </div>
         <div class="job">
-          <h2>{{ jobTitle }}</h2>
+          <h2>{{ User.jobTitle }}</h2>
         </div>
       </section>
       <section class="info">
         <ul>
-          <li>{{ gender }} / {{ birthdate }}</li>
-          <li>{{ university }}</li>
-          <li>{{ degree }} / {{ graduationYear }}年毕业</li>
-          <li>工作经验: {{ experienceYears }}年</li>
+          <li>{{ User.gender }} / {{ User.birthdate }}</li>
+          <li>{{ User.university }}</li>
+          <li>{{ User.degree }} / {{ User.graduationYear }}年毕业</li>
+          <li>工作经验: {{ new Date().getFullYear() - Number(User.graduationYear) }}年</li>
         </ul>
       </section>
       <section class="contact">
         <ul>
-          <li v-for="(contact, index) in contacts" :key="index">
+          <li v-for="(contact, index) in User.contacts" :key="index">
             <a :href="contact.href">
               <span class="contact-link">{{ contact.text }}</span>
               <span class="iconfont" v-html="contact.icon"></span>
@@ -33,19 +33,10 @@
   
   <script setup>
 import { ref, onMounted } from 'vue';
+import User from '@/config/user.json'; // 导入 JSON 文件
+
 const lastUpdated = ref(null);
 
-  defineProps({
-    name: String,
-      jobTitle: String,
-      gender: String,
-      birthdate: String,
-      university: String,
-      degree: String,
-      graduationYear: String,
-      experienceYears: Number,
-      contacts: Array
-  })
 
 
   onMounted(() => {
